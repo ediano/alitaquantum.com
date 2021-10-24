6f3f183abcd23b3714d1929d6c56d657cf4243da
Ediano Silva Santos <silva.ediano.santos@gmail.com>
2021-10-23 23:19:49 -0400

    update footer

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components'
 import { shade } from 'polished'
-import { Container as ContainerBase } from 'styles/layout'
+import { Container as ContainerBase, lessThan } from 'styles/layout'
 
 export const Container = styled.footer`
   ${({ theme }) => css`
@@ -11,7 +11,53 @@ export const Container = styled.footer`
 `
 
 export const Wrapper = styled(ContainerBase)`
+  display: grid;
+  grid-template-columns: 100px 1fr;
+  justify-items: end;
+
+  ${({ theme }) => css`
+    padding: 0 ${theme.spacing.xxl};
+    color: ${theme.colors.whiteIce};
+  `}
+
+  ${lessThan('m')(css`
+    grid-template-columns: 1fr;
+    justify-items: center;
+  `)}
+`
+
+export const Block = styled.div`
+  ${({ theme }) => css`
+    padding: ${theme.spacing.l};
+
+    & + & {
+      border-top: 1px solid ${theme.colors.secondary};
+    }
+  `}
+`
+
+export const Logo = styled.div`
+  position: relative;
+  width: 100px;
+  height: 100px;
+`
+
+export const ListLinks = styled.div``
+
+export const Anchor = styled.a`
+  display: table;
+
+  transition: 0.2s;
+
   ${({ theme }) => css`
     color: ${theme.colors.whiteIce};
+
+    & + & {
+      margin-top: ${theme.spacing.s};
+    }
+
+    &:hover {
+      color: ${shade(0.3, theme.colors.white)};
+    }
   `}
 `
