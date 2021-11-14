5a21f4908368b025460fe41e55da588387c7151f
Ediano Silva Santos <silva.ediano.santos@gmail.com>
2021-11-14 09:59:35 -0400

    update Analytics

---
+++ src/lib/gtag.ts
@@ -0,0 +1,22 @@
+export const GA_TRACKING_ID = 'UA-171838425-1'
+
+type Event = {
+  action: string
+  category: string
+  label: string
+  value: number
+}
+
+export const pageView = (url: URL): void => {
+  window.gtag('config', GA_TRACKING_ID, {
+    page_path: url
+  })
+}
+
+export const event = ({ action, category, label, value }: Event): void => {
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value
+  })
+}
