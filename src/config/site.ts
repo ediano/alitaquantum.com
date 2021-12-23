55af2a6740e2209ab6ac7702ea8110842739ae9b
Ediano Silva Santos <silva.ediano.santos@gmail.com>
2021-12-23 07:19:02 -0400

    update

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || '3000'
 
 export const site = {
   name: 'Alita Quantum',
-  title: 'Sua plataforma de troca de criptomoedas',
+  title: 'Plataforma de troca de criptomoedas',
   description: 'Troque criptomoedas de forma simples, fácil e sem limites.',
   keywords:
     'trocar, exchange, trocar instantaneamente, transferir, transferir instantaneamente, exchange instantaneamente, criptomoedas, plataforma de criptomoedas, taxa zero, troca sem custódia, troca privada, troca segura, transferir criptomoedas, btc, eth, trx, bnb',
