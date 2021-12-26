a56d2b1346a98a1e422563c31fe093bb7d932cd5
Ediano Silva Santos <silva.ediano.santos@gmail.com>
2021-12-26 11:26:36 -0400

    update key ticker

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -10,8 +10,8 @@ type Props = {
 export const Select = ({ name, currencies }: Props) => {
   return (
     <S.Container id={name}>
-      {currencies?.map((currency, index) => (
-        <S.Option key={currency.ticker + index} value={currency.name}>
+      {currencies?.map((currency) => (
+        <S.Option key={currency.ticker} value={currency.name}>
           {currency.ticker?.toUpperCase()}
         </S.Option>
       ))}
