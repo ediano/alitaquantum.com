d13ca166b8d4700bd70bc2b06b2afd4a32007c5a
Ediano Silva Santos <silva.ediano.santos@gmail.com>
2022-04-04 08:53:48 -0400

    add google optimize

--- src/components/GoogleOptimize/index.tsx
+++ src/components/GoogleOptimize/index.tsx
@@ -3,7 +3,7 @@ import Script from 'next/script'
 export const GoogleOptimize = () => {
   return (
     <Script
-      strategy="lazyOnload"
+      strategy="beforeInteractive"
       src={`https://www.googleoptimize.com/optimize.js?id=${process.env.NEXT_PUBLIC_GOOGLE_OPTIMIZE}`}
     />
   )
